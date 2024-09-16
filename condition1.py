a04866dbcb389cbfec5acd2876c27fa4550ee93a
rajtjoseph <rajtjoseph@gmail.com>
2024-09-16 11:53:17 -0500

    python-conditionals

---
+++ condition1.py
@@ -0,0 +1,6 @@
+x = float(input("Give first number : "))
+y = float(input("Give Second Number :"))
+if (x > y):
+    print(x, "  is greater than ", y)
+else:
+    print(y, " is greater or equal to ", x)
